754c440f99f6ddb4642a1130cfc09bc3d282d795
Eric Kennedy <ekeric13@gmail.com>
2014-12-30 14:06:47 -0800

    Implemented part of rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,13 +3,17 @@ var app = {}
 var query = window.location.search;
 app.username = _.escape(query.substring(query.indexOf('=') + 1, query.length));
 app.userFriends = [];
+app.rooms = [];
+app.currentRoom;
 
 app.server = 'https://api.parse.com/1/classes/chatterbox'
 
 app.init = function(){
   app.bindEvents();
-  app.fetch();
-  app.refreshMessageList();
+  app.fetch(function(data){
+    app.addMessages(data);
+    app.loadRooms(data);
+  });
 }
 
 app.send = function(message){
@@ -28,20 +32,27 @@ app.send = function(message){
   });
 }
 
-app.fetch = function(){
+app.fetch = function(callback){
   $.ajax({
     url: app.server,
     type: 'GET',
     data : {'order':'-createdAt'},
-    success: app.addAllMessages,
+    success: callback,
     error: function(data) {
       console.log("data error", data)
     }
   });
 }
 
-app.addAllMessages = function(data){
-  _.each(data.results.reverse(), app.addMessage);
+app.addMessages = function(data, roomFilter){
+  if (!roomFilter){
+    _.each(data.results.reverse(), app.addMessage);
+  } else {
+    var filtered = _.filter(data.results.reverse(), function(message){
+      return message["roomname"] === roomFilter
+    })
+    _.each(filtered, app.addMessage);
+  }
 }
 
 app.clearMessages = function(){
@@ -59,18 +70,11 @@ app.addMessage = function(message) {
   $("#chats").prepend(messageToAdd);
 }
 
-app.refreshMessageList = function(){
-  setInterval(function(){
-    app.clearMessages();
-    app.fetch();
-  }, 5000);
-}
-
 app.handleSubmit = function(e) {
   e.preventDefault();
   var messageText = $("#send input").val()
   app.addMessage({username: app.username, text: messageText});
-  app.send({username: app.username, text: messageText, roomname: "roomname"});
+  app.send({username: app.username, text: messageText, roomname: app.currentRoom});
   $("#send input#message-value").val("");
 };
 
@@ -87,9 +91,81 @@ app.addFriend = function(e) {
   app.userFriends.push(friendUsername);
 }
 
+app.addRoom = function(e) {
+  e.preventDefault();
+
+  var roomname = $("#new-room input").val()
+  if (app.rooms.indexOf(roomname) === -1 ){
+    app.rooms.push(roomname);
+    var temp = _.template("<li> <a href='#' class='room'> <%- roomname %> </a> </li>")
+    var roomToAdd = temp({roomname: roomname})
+    $("#roomSelect").append(roomToAdd);
+  } else {
+    alert('Roomname already exists!');
+  }
+
+  app.currentRoom = roomname;
+  $("#new-room input#room-value").val("");
+}
+
+app.filterRoomMessages = function(e){
+  var roomname = e.target.innerText;
+
+  // clear message
+  app.clearMessages();
+  // highlight roomname
+
+  app.currentRoom = roomname;
+  app.fetch(function(data) {
+    app.addMessages(data, app.currentRoom);
+  });
+
+}
+
+app.loadMessages = function(e){
+  e.preventDefault();
+  app.clearMessages();
+  app.fetch(function(data){
+    app.addMessages(data, app.currentRoom)
+  })
+}
+
+app.loadRooms = function(data) {
+  var temp = _.template("<li> <a href='#' class='room'> <%- roomname %> </a> </li>");
+  var rooms = [];
+
+  _.each(data.results, function(message){
+    rooms.push(message["roomname"])
+  })
+
+  var uniqueRooms = rooms.filter(function(room, index){
+    return rooms.indexOf(room) === index;
+  });
+
+  _.each(uniqueRooms, function(room){
+    if (!(room === undefined) && !(room === "")) {
+      var roomToAdd = temp({ roomname: room });
+      $('#roomSelect').append(roomToAdd);
+    }
+  })
+
+  // _.each(data.results, function(message) {
+  //   // duplicate roomnames and null values
+  //   console.log(message['roomname'])
+  //   // our app.rooms is an empty array i think. it is only the ones we created. data.results gives us all the rooms in the from the chatroom
+  //   if (!(message['roomname'] === undefined) || app.rooms.indexOf(message['roomname']) >= 0) {
+  //     // var roomToAdd = temp({ roomname: message['roomname'] });
+  //     // $('#roomSelect').append(roomToAdd);
+  //   }
+  // });
+};
+
 app.bindEvents = function() {
   $(document).on('submit', '#send', app.handleSubmit);
   $(document).on('click', '.username', app.addFriend);
+  $(document).on("submit", "#new-room", app.addRoom);
+  $(document).on("click", ".room", app.filterRoomMessages)
+  $(document).on("click", "#load-messages", app.loadMessages)
 };
 
 $(function() {
